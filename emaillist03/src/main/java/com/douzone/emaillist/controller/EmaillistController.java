79c062d793438e0628583467fe96038fe5b7be10
YUJIN <yujinj704@gmail.com>
2022-06-03 15:02:27 +0900

    commit

--- emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -2,9 +2,10 @@ package com.douzone.emaillist.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.douzone.emaillist.repository.EmaillistRepository;
 import com.douzone.emaillist.vo.EmaillistVo;
@@ -12,11 +13,15 @@ import com.douzone.emaillist.vo.EmaillistVo;
 @Controller
 public class EmaillistController {
 	
-	@ResponseBody
+	//1.주입 성공여부확인 //container의 기능적인 측면에서는 wired라고 함(DI관계인지 ask)
+	@Autowired
+	private EmaillistRepository emaillistRespository;
+	
+	//2.
 	@RequestMapping("")
-	public String index(){
-		List<EmaillistVo> list =new EmaillistRepository().findAll();
-		model.addAttribute();
+	public String index(Model model){
+		List<EmaillistVo> list = emaillistRespository.findAll();
+		model.addAttribute("list", list);
 		return "/WEB-INF/views/index.jsp";
 	}
 
